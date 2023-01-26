a7fb823949cf111e25a108c71e185361828dcb4d
Pandobeer <nikolyav@gmail.com>
2023-01-26 23:10:12 +0100

    feat: add profile tests

--- src/routes/profiles/index.ts
+++ src/routes/profiles/index.ts
@@ -9,7 +9,8 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
   fastify.get('/', async function (request, reply): Promise<
     ProfileEntity[]
   > {
-    return reply.send();
+    const profiles = await fastify.db.profiles.findMany();
+    return reply.send(JSON.stringify(profiles));
   });
 
   fastify.get(
@@ -20,7 +21,15 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      return reply.send();
+      const profileId = request.params.id;
+
+      const profile = await fastify.db.profiles.findOne({ key: "id", equals: profileId });
+
+      if (!profile) {
+        reply.notFound("Profile of user does not exist");
+      }
+
+      return reply.send(JSON.stringify(profile));
     }
   );
 
@@ -32,7 +41,31 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      return reply.send();
+      const userId = request.body.userId;
+
+      const user = await fastify.db.users.findOne({ key: "id", equals: userId });
+
+      if (!user) {
+        throw reply.badRequest("User does not exist");
+      }
+
+      const memberTypeId = request.body.memberTypeId;
+
+      const memberType = await fastify.db.memberTypes.findOne({ key: "id", equals: memberTypeId });
+
+      if (!memberType) {
+        throw reply.badRequest("Member Type does not exist");
+      }
+
+      const profile = await fastify.db.profiles.findOne({ key: "userId", equals: userId });
+
+      if (profile) {
+        throw reply.badRequest("User profile already exists");
+      }
+
+      const createdProfile = await fastify.db.profiles.create(request.body);
+
+      return reply.send(JSON.stringify(createdProfile));
     }
   );
 
@@ -44,7 +77,17 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      return reply.send();
+      const profileId = request.params.id;
+
+      const profileToDel = await fastify.db.profiles.findOne({ key: "id", equals: profileId });
+
+      if (!profileToDel) {
+        throw reply.badRequest("Profile does not exist");
+      }
+      await fastify.db.profiles.delete(profileId);
+
+
+      return reply.send(JSON.stringify(profileToDel));
     }
   );
 
@@ -57,7 +100,21 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
       },
     },
     async function (request, reply): Promise<ProfileEntity> {
-      return reply.send();
+      const profileId = request.params.id;
+
+      const profileToUpdate = await fastify.db.profiles.findOne({ key: "id", equals: profileId });
+
+      if (!profileToUpdate) {
+        throw reply.badRequest("Profile does not exist");
+      }
+
+      const dataToUpdate = request.body;
+
+      const updatedProfile = { ...profileToUpdate, ...dataToUpdate };
+
+      await fastify.db.profiles.change(profileId, updatedProfile);
+
+      return reply.send(JSON.stringify(updatedProfile));
     }
   );
 };
